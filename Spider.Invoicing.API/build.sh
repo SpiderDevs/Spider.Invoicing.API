09d5793fbf9e0db55ebd23defd0c01e2485c280c
BigOsDev <bigosdev0@gmail.com>
2017-11-19 20:54:17 +0100

    fix build

--- Spider.Invoicing.API/build.sh
+++ Spider.Invoicing.API/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 imageName="spider-invoicing-api";
 docker stop $imageName || true && docker rmi -f $imageName || true
-docker image  build . --tag $imageName
+docker build . --tag $imageName
