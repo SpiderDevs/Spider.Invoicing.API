37b2be0213e49723a1c0507e618ca0596fb02cc1
BigOsDev <bigosdev0@gmail.com>
2018-02-18 00:17:00 +0100

    Invoicing ap settings fix

---
+++ Spider.Invoicing.API/Spider.Invoicing.API/Models/DomainSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spider.Invoicing.API.Models
+{
+    public class DomainSettings
+    {
+        public string InvoicingWebClientUrl { get; set; }
+        public string InvoicingApiUrl { get; set; }
+    }
+}
